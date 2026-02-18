7618a955e2f1cb05688f2eaa7d831c9b17e327c2
koskateryna-sudo <kos.kateryna@lll.kpi.ua>
2026-02-18 15:05:42 +0200

    Update questions.py

--- znohub_mvp/questions.py
+++ znohub_mvp/questions.py
@@ -8,8 +8,6 @@ def load_questions():
     csv_path = os.path.join(current_dir, "questions.csv")
     
     df = pd.read_csv(csv_path, sep=';', encoding='utf-8')
-    for col in ["option1", "option2", "option3", "option4", "answer"]:
-        df[col] = df[col].astype(str).str.replace(" ⭐️", "", regex=False).str.strip()
     return df
     
 def get_topic_questions(questions):
