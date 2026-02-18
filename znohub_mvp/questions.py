6b98f3ee60bc8ab84124d641128e6fc99903805f
koskateryna-sudo <kos.kateryna@lll.kpi.ua>
2026-02-18 15:01:58 +0200

    Update questions.py

--- znohub_mvp/questions.py
+++ znohub_mvp/questions.py
@@ -4,10 +4,13 @@ import streamlit as st
 
 @st.cache_data
 def load_questions():
-    df = pd.read_csv("questions.csv", sep=';', encoding='utf-8')
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    csv_path = os.path.join(current_dir, "questions.csv")
+    
+    df = pd.read_csv(csv_path, sep=';', encoding='utf-8')
     for col in ["option1", "option2", "option3", "option4", "answer"]:
         df[col] = df[col].astype(str).str.replace(" ⭐️", "", regex=False).str.strip()
     return df
-
+    
 def get_topic_questions(questions):
     return questions[questions['topic'] == st.session_state['topic']].reset_index(drop=True)
